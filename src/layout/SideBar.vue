a31caaeae9d1f0ca5f13af33b5c8a160be78b788
MiMi <janicehsu890228@gmail.com>
2023-07-30 22:07:06 +0800

    新增sidebar至其他頁面所有的路徑

--- src/layout/SideBar.vue
+++ src/layout/SideBar.vue
@@ -6,51 +6,102 @@ const router = useRouter();
 const open = ref(['Member']);
 
 const memberGroup = ref('Member');
-const memberItem = ref([
-  '會員',
-  '感謝函',
-]);
-
-const newsGroup = ref('New')
-const newsItem = ref('消息管理')
+const memberItem = reactive(
+  [
+    {
+      id: "member-info",
+      name: "會員"
+    },
+    {
+      id: "thanks-letter",
+      name: "感謝函"
+    }
+  ]
+)
+const newsGroup = ref('News')
+const newsItem = reactive(
+  [
+    {
+      id: "news",
+      name: "消息管理"
+    }
+  ])
 
 const sponsorGroup = ref('Sponsor');
-const sponsorItem = ref([
-  '認養據點',
-  '認養訂單',
-]);
-
+const sponsorItem = reactive(
+  [
+    {
+      id: "sponsor-location",
+      name: "認養據點"
+    },
+    {
+      id: "sponsor-order",
+      name: "認養訂單"
+    }
+  ]
+)
 const donationGroup = ref('Donation');
-const donationItem = reactive([
-  {
-    id: "donate-project",
-    name: "捐款專案"
-  },
-  {
-    id: "donate-order",
-    name: "捐款訂單"
-  }]
-  // '捐款專案',
-  // '捐款訂單'
+const donationItem = reactive(
+  [
+    {
+      id: "donate-project",
+      name: "捐款專案"
+    },
+    {
+      id: "donate-order",
+      name: "捐款訂單"
+    }
+  ]
 )
 
 const activityGroup = ref('Activity');
-const activityItem = ref([
-  '星火活動',
-  '夢想之星',
-  '夢想之星投票',
-  '夢想之星留言'
-])
+const activityItem = reactive(
+  [
+    {
+      id: "spark-activity",
+      name: "星火活動"
+    },
+    {
+      id: "dream-star",
+      name: "夢想之星"
+    },
+    {
+      id: "dream-star-vote",
+      name: "夢想之星投票"
+    },
+    {
+      id: "message-board",
+      name: "星火活動留言"
+    }
+  ]
+)
 
 const resultGroup = ref('Result');
-const resultItem = ref([
-  '溫馨事紀',
-  '歷年報告',
-  '服務里程碑',
-])
+const resultItem = reactive(
+  [
+    {
+      id: "story",
+      name: "溫馨事紀"
+    },
+    {
+      id: "reports",
+      name: "歷年報告"
+    },
+    {
+      id: "milestone",
+      name: "服務里程碑"
+    }
+  ]
+)
 
 const backstageGroup = ref('Backstage');
-const backstageItem = ref('後台人員')
+const backstageItem = reactive(
+  [
+    {
+      id: "cms-staff",
+      name: "後台人員"
+    }
+  ])
 
 </script>
 
@@ -59,7 +110,9 @@ const backstageItem = ref('後台人員')
     <v-layout>
       <v-navigation-drawer permanent width="250">
         <div class="logo">
-          <img :src="'pictures/logo/logo_white.svg'" alt="星火logo">
+          <img :src="'pictures/logo/logo_white.svg'" alt="星火logo" @click="router.push({
+            path: '/home'
+          })">>
         </div>
         <v-list :v-model="open" density="compact" class="mt-4">
           <v-list-group class="title mt-1 mb-1" :value="memberGroup">
@@ -68,22 +121,25 @@ const backstageItem = ref('後台人員')
                 <v-icon icon="mdi-chevron-down" color="#1D3D6C" size="large"></v-icon>
               </v-list-item>
             </template>
-            <v-list-item v-for="(title, i) in memberItem" :key="i" :title="title" :value="title"></v-list-item>
+            <v-list-item v-for="item in memberItem" :key="item.id" :value="item.id" :title="item.name" @click="router.push({ path: '/' + item.id })"></v-list-item>
           </v-list-group>
+
           <v-list-group class="title  mt-1" :value="newsGroup">
             <template v-slot:activator="{ props }">
-              <v-list-item v-bind="props" :title="newsItem">
+              <v-list-item v-bind="props" v-for="item in newsItem" :key="item.id" :value="item.id" :title="item.name" @click="router.push({ path: '/' + item.id })">
               </v-list-item>
             </template>
           </v-list-group>
+
           <v-list-group class="title  mt-1" :value="sponsorGroup">
             <template v-slot:activator="{ props }">
               <v-list-item v-bind="props" title="認養管理">
                 <v-icon icon="mdi-chevron-down" color="#1D3D6C" size="large"></v-icon>
               </v-list-item>
             </template>
-            <v-list-item v-for="(title, i) in sponsorItem" :key="i" :value="title" :title="title"></v-list-item>
+            <v-list-item v-for="item in sponsorItem" :key="item.id" :value="item.id" :title="item.name" @click="router.push({ path: '/' + item.id })"></v-list-item>
           </v-list-group>
+
           <v-list-group class="title mt-1" :value="donationGroup">
             <template v-slot:activator="{ props }">
               <v-list-item v-bind="props" title="捐款管理">
@@ -91,36 +147,37 @@ const backstageItem = ref('後台人員')
               </v-list-item>
             </template>
 
-
             <v-list-item v-for="item in donationItem" :key="item.id" :value="item.id" :title="item.name"
               @click="router.push({ path: '/' + item.id })">
-
             </v-list-item>
 
           </v-list-group>
+
           <v-list-group class=" title mt-1" :value="activityGroup">
             <template v-slot:activator="{ props }">
               <v-list-item v-bind="props" title="活動管理">
                 <v-icon icon="mdi-chevron-down" color="#1D3D6C" size="large"></v-icon>
               </v-list-item>
             </template>
-            <v-list-item v-for="(title, i) in activityItem" :key="i" :value="title" :title="title"></v-list-item>
+            <v-list-item v-for="item in activityItem" :key="item.id" :value="item.id" :title="item.name" @click="router.push({ path: '/' + item.id })"></v-list-item>
           </v-list-group>
+
           <v-list-group class="title mt-1" :value="resultGroup">
             <template v-slot:activator="{ props }">
               <v-list-item v-bind="props" title="成果管理">
                 <v-icon icon="mdi-chevron-down" color="#1D3D6C" size="large"></v-icon>
               </v-list-item>
             </template>
-            <v-list-item v-for="(title, i) in resultItem" :key="i" :value="title" :title="title"></v-list-item>
+            <v-list-item v-for="item in resultItem" :key="item.id" :value="item.id" :title="item.name" @click="router.push({ path: '/' + item.id })"></v-list-item>
           </v-list-group>
+
           <v-list-group class="title mt-1" :value="backstageGroup">
             <template v-slot:activator="{ props }">
               <v-list-item v-bind="props" title="後台管理">
                 <v-icon icon="mdi-chevron-down" color="#1D3D6C" size="large"></v-icon>
               </v-list-item>
             </template>
-            <v-list-item :title="backstageItem"></v-list-item>
+            <v-list-item v-for="item in backstageItem" :key="item.id" :value="item.id" :title="item.name"  @click="router.push({ path: '/' + item.id })"></v-list-item>
           </v-list-group>
         </v-list>
         <v-sheet color="#1D3D6C" class="justify-center text-center bottom" width="250">
@@ -134,14 +191,13 @@ const backstageItem = ref('後台人員')
 
 <style scoped lang="scss">
 div.logo {
+  cursor: pointer;
   background-color: $primaryBgBlue;
-  width: 100%;
-  padding: 5%;
+  @include flex_hm;
+  padding: 8%;
 
   img {
     width: 80%;
-    display: block;
-    margin: auto;
   }
 }
 

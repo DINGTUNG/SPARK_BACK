8502d4fbd8098dc407e9ea67d31b842b24fc0f73
MiMi <janicehsu890228@gmail.com>
2023-08-01 22:57:01 +0800

    新增入口頁畫面(進行中2)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,24 +4,47 @@ const router = useRouter();
 </script>
 
 <template>
-  <div class="body">
-    <div class="title_block">
+  <div class="login_container">
+    <img :src="'pictures/login/shooting_star_white.svg'" alt="shooting_star_white" class="shooting_star_white">
+    <img :src="'pictures/login/logo.png'" alt="logo" class="logo">
+    <img :src="'pictures/login/shooting_star_gold.png'" alt="shooting_star_gold" class="shooting_star_gold">
+    <img :src="'pictures/login/three_stars.svg'" alt="three_stars" class="three_stars" >
 
-      <img :src="'pictures/login/shooting_star_white.svg'" alt="shoot" class="shooting_star_white">
 
-      <div class="title_text">
-        <img :src="'pictures/login/logo.png'" alt="logo" class="logo">
 
-        <img :src="'pictures/login/shoot.png'" alt="shoot" class="shoot">
+  </div>
 
-      </div>
 
-      </div>
-      <div>
-        <img class="star_img" :src="'pictures/login/star.svg'" alt="star">
-      </div>
-    </div>
 
+  <!-- <div class="login_wrap">
+
+      <h2>後台登入</h2>
+      <form action="" method="">
+        <label for="account">帳號</label>
+        <input type="text" class="account" v-model="account" placeholder="輸入您的帳號或信箱"
+          :class="{ 'animate__animated animate__headShake': errorAccount }" name="memId">
+        <label for="password">密碼</label>
+        <div class="password_wrapper" ref="passwordField"
+          :class="{ 'animate__animated animate__headShake': errorAccount }">
+          <div class="password_block">
+            <input :type="showPassword ? 'password' : 'text'" class="password" v-model="password" placeholder="輸入您的密碼"
+              name="memPsw">
+            <span class="toggle" @click="showHide"> <img v-if="showPassword" :src="'pictures/images/login/eye_hide.svg'"
+                alt="hide" />
+              <img v-else :src="'pictures/images/login/eye_show.svg'" alt="show" /></span>
+          </div>
+          <div class="recaptcha_forget_block">
+            <div class="forgot_psw">
+              <i class="fa-solid fa-circle-question"></i>
+              <a href="#">忘記密碼</a>
+            </div>
+          </div>
+        </div>
+        <div v-if="errorAccount" class="error_account">
+          {{ errorAccount }}
+        </div>
+      </form>
+    </div> -->
 </template>
 
 <style scoped lang="scss">

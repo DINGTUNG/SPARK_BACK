a4c1cb3cc69fc5ab8173e15c32f6cc4abfe52d59
njs-kayla <kayla881221@gmail.com>
2023-08-05 21:42:06 +0800

    新增溫馨事紀新增、刪除資料功能

--- src/views/pages/results/Story.vue
+++ src/views/pages/results/Story.vue
@@ -1,30 +1,46 @@
 <script setup>
-import popUpStory from '@/views/pop-ups/popUpStory.vue';
-import { ref, reactive,computed  } from 'vue'
+import PopUpStory from '@/views/pop-ups/popUpStory.vue';
+import { ref, reactive,computed } from 'vue'
+const dialogDelete = ref(false);
 const page = ref(1)
-const pageCount = () => {
-  return (storyList.length) / itemsPerPage + 1;
-}
-const itemsPerPage = 10;
 
-fetch('http://localhost/SPARK_BACK/php/results/story.php')
-  .then(res => res.clone().json())
-  .then(data => showStory(data))
-  .catch(err => console.log(err));
 
 let storyList = reactive([])
-const showStory = (data) => {
-  storyList = data.stories  
+
+  fetch('http://localhost/SPARK_BACK/php/results/story/read_story.php')
+    .then(res => res.json())
+    .then(data => {
+      storyList.value = data.stories
+    })
+    .catch(err => console.log(err))
+  
+    const itemsPerPage = 10;
+    const displayStoryList = computed(() => {
+      if (storyList.value) {
+        const startIdx = (page.value - 1) * itemsPerPage;
+        const endIdx = startIdx + itemsPerPage;
+        return reactive(storyList.value.slice(startIdx, endIdx));
+      } else {
+        return reactive([]);
+      }
+    });
+
+    const pageCount = () => {
+      return (displayStoryList.length) / itemsPerPage + 1;
+    };
+
+let deleteId = ref(null)
+const showDeleteDialog = (no) => {
+  dialogDelete.value = true
+  deleteId = no
 }
-
-
-// const displayStoryList = computed(() => {
-//   const startIdx = (page.value - 1) * itemsPerPage;
-//   const endIdx = startIdx + itemsPerPage;
-//   return storyList.slice(startIdx, endIdx);
-// });
-
-
+const closeDelete = () => {
+  dialogDelete.value = false
+}
+const deleteItemConfirm = () => {
+  window.location.assign(`http://localhost/SPARK_BACK/php/results/story/delete_story.php?story_no=${deleteId}`)
+}
+    
 
 
 </script>
@@ -48,13 +64,13 @@ const showStory = (data) => {
             <th>刪改</th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for="(item, index) in storyList" :key="item.story_no" class="no-border">
+        <tbody>  
+          <tr v-for="(item, index) in displayStoryList" :key="index" class="no-border">
             <td class="td_no">{{ ((page - 1) * itemsPerPage) + index + 1 }}</td>
-            <td class="id">{{ item.story_no }}</td>
+            <td class="id">{{ item.story_id }}</td>
             <td class="name">{{ item.story_title }}</td>
             <td class="start_date">{{ item.story_date }}</td>
-            <td class="online">{{ item.is_story_online ? '已上架' : '未上架' }}</td>
+            <td class="online">{{ item.is_story_online==1 ? '已上架' : '未上架' }}</td>
             <td>
               <v-switch v-model="item.is_story_online" color="#EBC483" density="compact" hide-details="true" inline
                 inset></v-switch>
@@ -63,22 +79,23 @@ const showStory = (data) => {
               <v-icon size="small" class="me-2" @click="editItem(item.raw)">
                 mdi-pencil
               </v-icon>
-              <v-icon size="small" @click="showDeleteDialog(item.raw)">mdi-delete</v-icon>
+              <v-icon size="small" @click="showDeleteDialog(item.story_no)">mdi-delete</v-icon>
             </td>
           </tr>
         </tbody>
+
       </v-table>
     </div>
     <PopUpStory class="add" />
 
       <!-- 分頁 -->
-      <!-- <div class="text-center">
+      <div class="text-center">
         <v-pagination v-model="page" :length=pageCount() rounded="circle" prev-icon="mdi-chevron-left"
           next-icon="mdi-chevron-right" active-color="#F5F4EF" color="#E7E6E1"></v-pagination>
-      </div> -->
+      </div>
     </div>
 
-    <v-dialog v-model="dialogDelete" max-width="800px" persistent="true">
+    <v-dialog v-model="dialogDelete" max-width="800px" persistent>
 
       <v-card class="delete_dialog">
         <v-card-title class="text-center">
@@ -90,13 +107,12 @@ const showStory = (data) => {
             取消
           </v-btn>
           <v-btn color="#F2DFBF" variant="text" @click="deleteItemConfirm">
-            刪除
+             刪除
           </v-btn>
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <popUpStory/>
 
 
 

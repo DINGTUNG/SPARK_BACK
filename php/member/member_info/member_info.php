62fa8c18ee15e83bb6b09dad8cae0bb6d46708ce
yiiijie <qaq2104602@gmail.com>
2023-08-08 00:15:48 +0800

    會員資料串接資料庫完成（詳細的會員資料尚未）

---
+++ php/member/member_info/member_info.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *");//標頭修改
+ require_once("../../connect_chd102g3.php");
+try{
+  $sql = "select * from member_info";
+  $location=$pdo->prepare($sql);
+  $location->execute();
+  
+  if( $location->rowCount() == 0 ){ //找不到
+    //傳回空的JSON字串
+      echo"{}";
+  }else{ //找得到
+    //取回一筆資料
+    $localRow=$location->fetchAll(PDO::FETCH_ASSOC);
+    //送出json字串
+    echo json_encode($localRow);
+  }	
+}catch(PDOException $e){
+  echo $e->getMessage();
+}
+?>
--- src/views/pages/member/MemberInfo.vue
+++ src/views/pages/member/MemberInfo.vue
@@ -20,22 +20,22 @@ const displayedMemberInfo = computed(() => {
 
 
 const MemberInfo = reactive([])
-// async function newsConnection() {
-//   try {
-//     const response = await axios.post('http://localhost/SPARK_BACK/php/news/news.php')
-//     console.log(response)
-//     if (response.data.length > 0) {
-//       response.data.forEach(element => {
-//         newsList.push(element)
-//       });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// onMounted(() => {
-//   newsConnection()
-// })
+async function newsConnection() {
+  try {
+    const response = await axios.post('http://localhost:8888/member/member_info/member_info.php')
+    console.log(response)
+    if (response.data.length > 0) {
+      response.data.forEach(element => {
+        MemberInfo.push(element)
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+onMounted(() => {
+  newsConnection()
+})
 </script>
 
 
@@ -65,9 +65,15 @@ const MemberInfo = reactive([])
           <tbody>
             <tr v-for="(item, index) in displayedMemberInfo" :key="item.id" class="no-border">
               <td class="td_no">{{ ((page - 1) * itemsPerPage) + index + 1 }}</td>
-              <td class="td_id">{{ item.news_id }}</td>
-              <td class="name">{{ item.news_title }}</td>
-              <td class="date">{{ item.news_date }}</td>
+              <td class="td_id">{{ item.member_id }}</td>
+              <td class="name">{{ item.member_name }}</td>
+              <td class="salutation">{{ item.member_salutation }}</td>
+              <td class="account">{{ item.member_account }}</td>
+              <td class="mobile">{{ item.member_mobile }}</td>
+              <td class="home_phone">{{ item.member_home_phone }}</td>
+              <td class="business_phone">{{ item.member_business_phone }}</td>
+              <td class="address">{{ item.member_address }}</td>
+              <td class="receipt_class">{{ item.receipt_class }}</td>
             </tr>
           </tbody>
         </v-table>

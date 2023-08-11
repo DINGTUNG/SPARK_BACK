86979cc0e5386b95be58dedd3e9dddbfedbd6766
lockon1248 <tom5678tom5678t@yahoo.com.tw>
2023-08-11 17:50:17 +0800

    修正後台報告頁上下架功能

--- src/stores/results/reports.js
+++ src/stores/results/reports.js
@@ -38,52 +38,98 @@ export const useReportStore = defineStore('Report', () => {
             }
         }
     }
-    //up
-    function updateReportBackend(reportNo,reportClass,reportTitle,reportFilePath) {
-        // prepare data 
-        const payLoad = new FormData();
-        payLoad.append("report_no", reportNo);
-        payLoad.append("report_Class", reportClass);
-        payLoad.append("report_Title", reportTitle);
-        payLoad.append("reports_file_path", reportFilePath);
-    
-        // make a request
-        const request = {
-          method: "POST",
-          url: `http://localhost/SPARK_BACK/php/activity/message-board/update_message.php`,
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-          data: payLoad,
-        };
-    
-        // send request to backend server
-        return new Promise((resolve, reject) => {
-          axios(request)
-            .then((response) => {
-              const updateResult = response.data;
-              resolve(updateResult);
-            })
-            .catch((error) => {
-              console.log("From updateMessageBackend:", error);
-              reject(error);
-            });
-        });
-      }
-    
-      const updateMessageFromMessagePool = (messageNo,sparkActivityId,messageContent,memberId) => {
-        for (let i = 0; i < messagePool.length; i++) {
-          if (messagePool[i].message_no == messageNo) {
-          messagePool[i].spark_activity_id = sparkActivityId
-          messagePool[i].message_content = messageContent
-          messagePool[i].member_id = memberId
-          }
+
+    //status
+    function updateReportOnlineBackend(reportNo,reportOnline) {
+      // prepare data 
+      const payLoad = new FormData();
+      payLoad.append("report_no", reportNo);
+      payLoad.append("is_report_online", reportOnline);
+  
+      // make a request
+      const request = {
+        method: "POST",
+        url: `http://localhost/SPARK_BACK/php/results/reports/reports_status.php`,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        data: payLoad,
+      };
+  
+      // send request to backend server
+      return new Promise((resolve, reject) => {
+        axios(request)
+          .then((response) => {
+            const updateResult = response.data;
+            resolve(updateResult);
+          })
+          .catch((error) => {
+            console.log("From updateReportOnlineBackend:", error);
+            reject(error);
+          });
+      });
+    }
+  
+    const updateReportFromReportsList = (reportNo,reportOnline) => {
+      for (let i = 0; i < reportsList.length; i++) {
+        if (reportsList[i].report_no == reportNo) {
+        reportsList[i].is_report_online = reportOnline
         }
       }
+    }
+
+
+    //up
+    // function updateReportBackend(reportNo,reportClass,reportTitle,reportFilePath) {
+    //     // prepare data 
+    //     const payLoad = new FormData();
+    //     payLoad.append("report_no", reportNo);
+    //     payLoad.append("report_Class", reportClass);
+    //     payLoad.append("report_Title", reportTitle);
+    //     payLoad.append("reports_file_path", reportFilePath);
+    
+    //     // make a request
+    //     const request = {
+    //       method: "POST",
+    //       url: `http://localhost/SPARK_BACK/php/activity/message-board/update_message.php`,
+    //       headers: {
+    //         "Content-Type": "multipart/form-data",
+    //       },
+    //       data: payLoad,
+    //     };
+    
+    //     // send request to backend server
+    //     return new Promise((resolve, reject) => {
+    //       axios(request)
+    //         .then((response) => {
+    //           const updateResult = response.data;
+    //           resolve(updateResult);
+    //         })
+    //         .catch((error) => {
+    //           console.log("From updateMessageBackend:", error);
+    //           reject(error);
+    //         });
+    //     });
+    //   }
+    
+    //   const updateMessageFromMessagePool = (messageNo,sparkActivityId,messageContent,memberId) => {
+    //     for (let i = 0; i < messagePool.length; i++) {
+    //       if (messagePool[i].message_no == messageNo) {
+    //       messagePool[i].spark_activity_id = sparkActivityId
+    //       messagePool[i].message_content = messageContent
+    //       messagePool[i].member_id = memberId
+    //       }
+    //     }
+    //   }
+
+
+
 
     return {
         reportsList,
         deleteReportBackend,
         deleteReportFromMessagePool,
+        updateReportOnlineBackend,
+        updateReportFromReportsList
     }
 })
--- src/views/pages/sponsor/SponsorLocation.vue
+++ src/views/pages/sponsor/SponsorLocation.vue
@@ -7,7 +7,7 @@ import { ref, computed, onMounted } from 'vue';
 import axios from 'axios';
 import { useSponsorLocationStore } from '@/stores/sponsor/sponsor-location.js';
 const locationStore = useSponsorLocationStore();
-
+//api
 async function getSponsorLocation() {
   try {
     const response = await axios.post('http://localhost/SPARK_BACK/php/sponsor/sponsor-location/get_sponsor_location.php');
@@ -26,6 +26,7 @@ onMounted(() => {
   getSponsorLocation();
 });
 
+//分頁
 const page = ref(1);
 const itemsPerPage = 10;
 const pageCount = computed(() => {
@@ -37,6 +38,8 @@ const displayLocationList = computed(() => {
   return filteredLocationList.value.slice(startIdx, endIdx);
 });
 
+
+//搜索
 const searchValue = ref('');
 function handleSearchChange(newValue) {
   searchValue.value = newValue;
@@ -57,6 +60,23 @@ const filteredLocationList = computed(() => {
     return str.includes(searchText.value)
   });
 });
+
+//上下架
+async function UpdateLocationOnline(item) {
+  try {
+    if (item.location_no == null) {
+      throw new Error("location no not found!")
+    }
+    await locationStore.updateLocationOnlineBackend(item.location_no,item.is_sponsor_location_online)
+    locationStore.updateOrderStatusFromLocationList(item.location_no,item.is_sponsor_location_online)
+
+    // console.log(item.is_sponsor_location_online);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 
 </script>
 
@@ -89,7 +109,8 @@ const filteredLocationList = computed(() => {
               <td class="online">{{ item.is_sponsor_location_online == 1 ? '已上架' : '未上架' }}</td>
               <td>
                 <v-switch v-model="item.is_sponsor_location_online" color="#EBC483" density="compact" hide-details="true"
-                  inline inset true-value=1></v-switch>
+                  inline inset true-value=1 @change="UpdateLocationOnline(item)">
+                </v-switch>
               </td>
               <td class="year">{{ item.updater }}</td>
               <td class="name">{{ item.update_time }}</td>

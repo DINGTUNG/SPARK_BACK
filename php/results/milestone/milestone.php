f9ca2563e096830022e2ae15cd05cc582fb278af
dos8742 <zxc897004@gmail.com>
2023-08-11 17:01:31 +0800

    「服務里程碑」 刪除功能已完成

--- php/results/milestone/milestone.php
+++ php/results/milestone/milestone.php
@@ -1,21 +1,21 @@
 <?php
-header("Access-Control-Allow-Origin: *");//標頭修改
- require_once("../../connect_chd102g3.php");
-try{
-  $sql = "select * from milestone";
-  $location=$pdo->prepare($sql);
-  $location->execute();
-  
-  if( $location->rowCount() == 0 ){ //找不到
-    //傳回空的JSON字串
-      echo"{}";
-  }else{ //找得到
-    //取回一筆資料
-    $localRow=$location->fetchAll(PDO::FETCH_ASSOC);
-    //送出json字串
-    echo json_encode($localRow);
-  }	
-}catch(PDOException $e){
-  echo $e->getMessage();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+require_once("../../connect_chd102g3.php");
+
+try {
+  $sql = "
+  select * from milestone 
+  where del_flg = 0 order by milestone_no";
+  $milestone = $pdo->query($sql);
+
+  //----------------------------------------
+  $milestoneRow = $milestone->fetchAll(PDO::FETCH_ASSOC);
+  echo json_encode($milestoneRow);
+} catch (Exception $e) {
+  echo "錯誤行號 : ", $e->getLine(), "<br>";
+  echo "錯誤原因 : ", $e->getMessage(), "<br>";
+  echo "系統暫時不能正常運行，請稍後再試<br>";
 }
-?>

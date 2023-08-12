1795d6b90284f82697f597f653e2cf08641d1493
njs-kayla <kayla881221@gmail.com>
2023-08-12 18:40:17 +0800

    新增登入相關 api & 修改故事藝廊寫法

--- php/results/story/read_story.php
+++ php/results/story/read_story.php
@@ -4,12 +4,13 @@
     try{
         $sql = "SELECT * FROM story WHERE del_flg=0 ORDER BY story_no DESC";
         $result = $pdo->query($sql);
-        $res = json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-        echo $res;
+        $res = $result->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($res);
     }
     catch(PDOException $e){
         echo "錯誤行號 : ", $e->getLine(), "<br>";
         echo "錯誤原因 : ", $e->getMessage(), "<br>";
         echo "系統暫時不能正常運行，請稍後再試<br>";
+        error_log($e->getMessage());
     }
 ?>

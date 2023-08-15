6a53fac1f8f9ca50116f9b92b5bea154635ff8e5
MiMi <janicehsu890228@gmail.com>
2023-08-15 20:30:07 +0800

    Auto stash before merge of "dev" and "origin/dev"

--- php/member/thanks-letter/get_thanks_letter.php
+++ php/member/thanks-letter/get_thanks_letter.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:8888");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
---
+++ php/sponsor/sponsor-location/get_sponsor_location_front.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: https://tibamef2e.com");//緯育
+// header("Access-Control-Allow-Origin: http://localhost:5174");//本地端
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+require_once("../../connect_chd102g3.php");
+
+try {
+  $sql = "
+  select * from sponsor_location 
+  where del_flg = 0 and is_sponsor_location_online = 1";
+  $sponsor_location = $pdo->query($sql);
+
+  //----------------------------------------
+  $sponsor_locationRow = $sponsor_location->fetchAll(PDO::FETCH_ASSOC);
+  echo json_encode($sponsor_locationRow);
+} catch (Exception $e) {
+  echo "錯誤行號 : ", $e->getLine(), "<br>";
+  echo "錯誤原因 : ", $e->getMessage(), "<br>";
+  echo "系統暫時不能正常運行，請稍後再試<br>";
+}

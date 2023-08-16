cb79b4d882d46acc1b744fab4c8eccfd8a5553b1
njs-kayla <kayla881221@gmail.com>
2023-08-17 05:22:00 +0800

    修改刪除故事&驗證信寄送

--- php/member/membership_system/verification_letter.php
+++ php/member/membership_system/verification_letter.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://tibamef2e.com/chd102/g3/");
+header("Access-Control-Allow-Origin: https://tibamef2e.com/");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
@@ -32,11 +32,11 @@ try {
     $mail->FromName = "=?UTF-8?B?" . base64_encode("星火兒童認養協會") . "?="; //設定寄件人名稱
     $mail->Subject = "=?UTF-8?B?" . base64_encode("星火兒童認養協會會員驗證信") . "?="; //設定郵件主題
     $mail->Body = "
-    <div style = 'background-color:#1d3d6c;'>
+    <div style = 'background-color:#1d3d6c; padding:30px 0;'>
     <div style = 'text-align:center; margin:20px 50px;padding:20px; color:#3d3a35; font-weight: bold; border-radius:15px; letter-spacing: 2px; background-color:#f5f4ef'>
         <p>親愛的&nbsp;".$member_account."<span><p>
         <br>
-        <p >星火協會感謝您的註冊！，我們誠摯的希望能與您共同守護貧困兒童的幸福未來</p>
+        <p >星火協會感謝您的註冊！我們誠摯的希望能與您共同守護貧困兒童的幸福未來</p>
         </br>
         <p>要驗證您的帳號請複製以下驗證碼: <span style =' color:#d8b06c; '>".$verification_code."</span> 至註冊頁面
         </p>
@@ -64,6 +64,4 @@ try {
 } catch ( PDOException $e ) {
     echo $e->getMessage();
   }
-
-
 ?>
--- php/results/story/delete_story.php
+++ php/results/story/delete_story.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../../connect_chd102g3.php");
-    $story_no = $_GET['story_no'];
+    $story_no = $_POST['story_no'];
     $sql = "UPDATE story SET del_flg=1, updater='希', updater_time=Now() WHERE story_no = ".$story_no;
 
     if ($pdo->query($sql)) {

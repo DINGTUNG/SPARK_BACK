79442e1af343155e77422c5bd7c8e4339723e2ac
njs-kayla <kayla881221@gmail.com>
2023-08-16 16:39:42 +0800

    驗證信樣式修改

--- php/member/membership_system/verification_letter.php
+++ php/member/membership_system/verification_letter.php
@@ -12,8 +12,9 @@ require '../../../PHPMailer/SMTP.php';
 
 try {
 
+
+    $member_account = $_GET["member_account"];
     $verification_code = uniqid();
-    $baseURL = "http://localhost:5174/chd102/g3/register?verification_code=";
 
 
     //設定SMTP
@@ -31,12 +32,14 @@ try {
     $mail->FromName = "=?UTF-8?B?" . base64_encode("星火兒童認養協會") . "?="; //設定寄件人名稱
     $mail->Subject = "=?UTF-8?B?" . base64_encode("星火兒童認養協會會員驗證信") . "?="; //設定郵件主題
     $mail->Body = "
-    <p>親愛的<span style = 'color:blue'>".$member_account."<span><p>
-    <br>
-    <p>星火協會感謝您的註冊！，我們誠摯的希望能與您共同守護貧困兒童的幸福未來</p>
-    </br>
-    <p>要驗證您的帳號請複製以下驗證碼: ".$verification_code." 至註冊頁面
-    </p>
+    <div style = 'text-align:center; margin:20px 50px;padding:20px; background-color:#bbd0f7; color:#3d3a35; font-weight: bold; border-radius:15px; letter-spacing: 2px;'>
+        <p>親愛的".$member_account."<span><p>
+        <br>
+        <p >星火協會感謝您的註冊！，我們誠摯的希望能與您共同守護貧困兒童的幸福未來</p>
+        </br>
+        <p>要驗證您的帳號請複製以下驗證碼: <span style =' color:#d8b06c; '>".$verification_code."</span> 至註冊頁面
+        </p>
+    </div>
     "
     ;  //設定郵件內容
     $mail->IsHTML(true);  //設定是否使用HTML格式

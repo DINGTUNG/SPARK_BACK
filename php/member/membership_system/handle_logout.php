5b0bf92528186c59152b340df5cc0be4cfddb961
DINGTUNG <jjwelkintung0124@gmail.com>
2023-08-16 17:51:57 +0800

    修復捐款專案編輯功能

--- php/member/membership_system/handle_logout.php
+++ php/member/membership_system/handle_logout.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");

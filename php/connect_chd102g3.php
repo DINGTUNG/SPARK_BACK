9d6dd990a89340d23086c14e2bf554bb8c709bcf
dos8742 <zxc897004@gmail.com>
2023-08-16 19:45:13 +0800

    測試localhost

--- php/connect_chd102g3.php
+++ php/connect_chd102g3.php
@@ -1,7 +1,6 @@
 <?php 
-// header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Origin: https://tibamef2e.com");//緯育
-// header("Access-Control-Allow-Origin: http://localhost:5173");//本地端
+header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: https://tibamef2e.com");//緯育
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
@@ -10,6 +9,12 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
     $user = "spark";
     $password = "666";
     $port = 3306;
+
+    // 緯育
+    // $dbname = "tibamefe_chd102g3"; //資料庫名稱
+    // $user = "tibamefe_since2021"; //帳號
+    // $password = "vwRBSb.j&K#E"; //密碼
+    // $port = 3306;
 
     //詢問資料庫建置位置
     $dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";

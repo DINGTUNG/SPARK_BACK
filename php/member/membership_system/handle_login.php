550a52d8e8cc348dc03f539c3ab03ea971c3ddb5
yiiijie <133617070+yiiijie@users.noreply.github.com>
2023-08-17 12:19:42 +0800

    登入 網址暫時改為localhost

--- php/member/membership_system/handle_login.php
+++ php/member/membership_system/handle_login.php
@@ -1,6 +1,7 @@
 <?php
-require_once("../../connect_chd102g3.php");
-header("Access-Control-Allow-Origin: https://tibamef2e.com/chd102/g3/");
+require_once("../../connect_chd102g3-yiiijie.php");
+// header("Access-Control-Allow-Origin: https://tibamef2e.com/chd102/g3/");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
